8954bf1afb5c9520da43661fa9f73d9afd54beb6
Christoph Hösler <christoph.hoesler@gmail.com>
2015-01-22 10:30:12 +0100

    Added rgb method

--- R/iwanthue.r
+++ R/iwanthue.r
@@ -2,36 +2,47 @@ NULL
 
 #' IWantHue palette generator.
 #'
-#' @field v8 The V8 engine context.
+#' @field v8 The V8 context.
 #' @export
 IWantHue <- setRefClass("IWantHue",
   fields = list(v8 = "ANY"),
   methods = list(
-  	initialize = function() {
-  		v8 <<- new_context();
+  	initialize = function(context) {
+  		v8 <<- context;
 		v8$source(system.file("chroma.js", package = "rwantshue"))
 		v8$source(system.file("chroma.palette-gen.js", package = "rwantshue"))
 		v8$source(system.file("lodash.js", package = "rwantshue"))
-  	},
-    hex = function(n = 8, force_mode = FALSE, quality = 50, ...) {
-	  	js_code <- sprintf("
+		v8$eval("iwanthue = function(n, force_mode, quality, js_color_mapper) {
 			filter_colors = function(color) {
 			    var hcl = color.hcl();
 			    return hcl[0]>=0 && hcl[0]<=360
 			      && hcl[1]>=0 && hcl[1]<=3
 			      && hcl[2]>=0 && hcl[2]<=1.5;
 			}
-			var colors = paletteGenerator.generate(%d, filter_colors, %s, %d);
+			var colors = paletteGenerator.generate(n, filter_colors, force_mode, quality);
 			colors = paletteGenerator.diffSort(colors);
-			colors = _.map(colors, function(color) { return color.hex(); });
-			JSON.stringify(colors);
-	    ", n, ifelse(force_mode, "true", "false"), quality)
-	  	fromJSON(v8$eval(js_code))
+			colors = _.map(colors, js_color_mapper);
+			return JSON.stringify(colors);
+		}")
+  	},
+  	iwanthue = function(n = 8, force_mode = FALSE, quality = 50, js_color_mapper) {
+  		json <- v8$call("iwanthue", n, force_mode, quality, js_color_mapper)
+  		fromJSON(json)
+  	},
+    hex = function(...) {
+	  	"Generate a vector of colors in hex format"
+	  	.self$iwanthue(..., js_color_mapper = I("function(color) { return color.hex(); }"))	  	
+	},
+	rgb = function(...) {
+		"Generate a matrix of colors in rgb format"
+		.self$iwanthue(..., js_color_mapper = I("function(color) { return color.rgb; }"))
 	}
   )
 )
 
+#' Create a new \linkS4class{IWantHue} object.
+#' 
 #' @export
 iwanthue <- function() {
-	IWantHue$new()
+	IWantHue$new(new_context())
 }
--- tests/testthat/test-iwanthue.r
+++ tests/testthat/test-iwanthue.r
@@ -1,6 +1,6 @@
 context("iwanthue")
 
-test_that("iwanthue creates a vector of hex values", {
+test_that("hex creates a vector of hex values", {
   # given 
   scheme <- iwanthue()
   
@@ -12,7 +12,7 @@ test_that("iwanthue creates a vector of hex values", {
   expect_match(palette, "#[0-9A-F]{6}")
 })
 
-test_that("iwanthue creates the correct number of colors", {
+test_that("hex creates the correct number of colors", {
   # given 
   scheme <- iwanthue()
   n <- 10
@@ -22,4 +22,29 @@ test_that("iwanthue creates the correct number of colors", {
 
   # then
   expect_equal(length(palette), n)
+})
+
+test_that("rgb creates a matrix of 3 rgb values", {
+  # given 
+  scheme <- iwanthue()
+  
+  # when
+  palette <- scheme$rgb()
+
+  # then
+  expect_is(palette, "matrix")
+  expect_true(is.numeric(palette))
+  expect_equal(ncol(palette), 3)
+})
+
+test_that("rgb creates the correct number of colors", {
+  # given 
+  scheme <- iwanthue()
+  n <- 10
+  
+  # when
+  palette <- scheme$rgb(n)
+
+  # then
+  expect_equal(nrow(palette), n)
 })
